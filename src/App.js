4cb85fdbaf5c849f49c84dc11416fd1a5cfecdeb
carlmagumpara <carlmagumpara@e-lawsolutions.com>
2019-01-18 15:33:27 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { 
+      itemArray: [
+        {
+          'name': 'fringe',
+          'zIndex': 10,
+        },
+        {
+          'name': 'topa',
+          'zIndex': 5,
+        },
+        {
+          'name': 'topb',
+          'zIndex': 6,
+        },
+        {
+          'name': 'bottom',
+          'zIndex': 4,
+        },
+        {
+          'name': 'back',
+          'zIndex': -10,
+        },
+        {
+          'name': 'eyes',
+          'zIndex': 2,
+        },
+        {
+          'name': 'eyebrows',
+          'zIndex': 11,
+        },
+        {
+          'name': 'mouth',
+          'zIndex': 11,
+        },
+        {
+          'name': 'dress',
+          'zIndex': 5,
+        },
+        {
+          'name': 'shoes',
+          'zIndex': 1,
+        },
+        {
+          'name': 'hat',
+          'zIndex': 11,
+        },
+        {
+          'name': 'acc',
+          'zIndex': 9,
+        },
+        {
+          'name': 'ltph',
+          'zIndex': 11,
+        },
+        {
+          'name': 'beard',
+          'zIndex': 7,
+        },
+        {
+          'name': 'eyewear',
+          'zIndex': 9,
+        },
+        {
+          'name': 'emotion',
+          'zIndex': 1,
+        },
+        {
+          'name': 'other',
+          'zIndex': 12,
+        },
+        {
+          'name': 'cape',
+          'zIndex': -1,
+        },
+        {
+          'name': 'scarf',
+          'zIndex': 8,
+        },
+        {
+          'name': 'base',
+          'zIndex': 0,
+        },
+      ],
+
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  doll() {
+    return (
+      <div>
+        {this.state.itemArray.map((doll) =>
+          <img 
+            key={doll.name} 
+            id={doll.name} 
+            alt={doll.name} 
+            className="doll" 
+            style={{ zIndex: doll.zIndex }}
+          />          
+        )}
+      </div>
+    );
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.doll()}
       </div>
     );
   }
